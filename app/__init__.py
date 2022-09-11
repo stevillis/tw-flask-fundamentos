14a17f9f54e57bf49b9956c6ed212d351404b72f
Stévillis Sousa <stevillis@hotmail.com>
2022-09-11 13:27:07 -0400

    feat: migrate cliente model

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 app = Flask(__name__)
@@ -9,4 +10,7 @@ app.config.from_object('config')
 
 db = SQLAlchemy(app)
 
+migrate = Migrate(app, db)
+
 from .views import cliente_view  # pylint: disable=wrong-import-position # noqa: 401, 402
+from .models import cliente_model
