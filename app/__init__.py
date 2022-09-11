82dc586ebf7753e1eab16ad2b985c080ef1cac60
Stévillis Sousa <stevillis@hotmail.com>
2022-09-11 03:12:42 -0400

    feat: configure postgres connection

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 """App config."""
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config.from_object('config')
 
-from .views import cliente_view # pylint: disable=wrong-import-position # noqa: 401, 402
+db = SQLAlchemy(app)
+
+from .views import cliente_view  # pylint: disable=wrong-import-position # noqa: 401, 402
