7abeda368d0e23442668a92a188eec9cfc05c27c
Stévillis Sousa <stevillis@hotmail.com>
2022-09-12 01:27:30 -0400

    feat: persist cliente data on database

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -1,14 +1,40 @@
 """Cliente view."""
 
-from app import app
+
+from app import app, db
 from app.forms import cliente_form
+from app.models import cliente_model
 from flask import render_template
 
 
-@app.route('/cadastrar_cliente', methods={'GET', 'POST'})
+@app.route('/cadastrar_cliente', methods=['GET', 'POST'])
 def cadastrar_cliente():
     """View for create cliente."""
+    print('entrou')
     form = cliente_form.ClienteForm()
+    if form.validate_on_submit():
+        nome = form.nome.data
+        email = form.email.data
+        data_nascimento = form.data_nascimento.data
+        profissao = form.profissao.data
+        sexo = form.sexo.data
+
+        cliente = cliente_model.Cliente(
+            nome=nome,
+            email=email,
+            data_nascimento=data_nascimento,
+            profissao=profissao,
+            sexo=sexo,
+        )
+
+        try:
+            db.session.add(cliente)
+            db.session.commit()
+        except:
+            print('Cliente não cadastrado.')
+    else:
+        print(form.errors)
+        print(form)
 
     return render_template('clientes/form.html', form=form)
 
