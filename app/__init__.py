24542b34cf7386bfdae6b4fe87a7c98085a3ddb9
Stévillis Sousa <stevillis@hotmail.com>
2022-09-14 22:23:28 -0400

    feat: translate form validation error messages

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -15,6 +16,8 @@ migrate = Migrate(app, db)
 
 csrf = CSRFProtect(app)
 csrf.init_app(app)
+
+babel = Babel(app)
 
 from .views import cliente_view  # pylint: disable=wrong-import-position # noqa: 401, 402
 from .models import cliente_model
