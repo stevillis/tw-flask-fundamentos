539c3a94b8c33c18582262df747e35b2835c7695
Stévillis Sousa <stevillis@hotmail.com>
2022-09-16 02:06:10 -0400

    feat: redirect to cliente's list after cliente creation

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -4,13 +4,12 @@
 from app import app, db
 from app.forms import cliente_form
 from app.models import cliente_model
-from flask import render_template
+from flask import redirect, render_template, url_for
 
 
 @app.route('/cadastrar_cliente', methods=['GET', 'POST'])
 def cadastrar_cliente():
     """View for create cliente."""
-    print('entrou')
     form = cliente_form.ClienteForm()
     if form.validate_on_submit():
         nome = form.nome.data
@@ -30,11 +29,11 @@ def cadastrar_cliente():
         try:
             db.session.add(cliente)
             db.session.commit()
+            return redirect(url_for('listar_clientes'))
         except:
             print('Cliente não cadastrado.')
     else:
         print(form.errors)
-        print(form)
 
     return render_template('clientes/form.html', form=form)
 
